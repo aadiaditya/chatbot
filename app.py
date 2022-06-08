b125e37f293a6430beeb2191c4a5ca5471d62e22
Sai Aditya Guntupalli <adityaguntupalli9999@gmail.com>
2022-06-08 15:21:11 +0530

    commit

--- app.py
+++ app.py
@@ -14,28 +14,21 @@ def bot():
     msg = bot_resp.message()
     # Applying bot logic
     if 'hello' in user_msg:
-        msg.body("Hi there! How may I help you?")
-    elif 'machine learning' in user_msg:
-        msg.body("Machine learning is the study of computer algorithms that can improve automatically through experience and by the use of data. It is seen as a part of artificial intelligence.")
-        msg.body('You can learn Machine Learning from here https://www.youtube.com/c/MachineLearningHub')
-    elif 'image processing' in user_msg:
-        msg.body("Image processing can be defined as the technical analysis of an image by using complex algorithms.")
-    elif 'natural language processing' in user_msg:
-        msg.body("Natural language processing is a subfield of linguistics, computer science, and artificial intelligence concerned with the interactions between computers and human language")
-    elif 'who' in user_msg:
-        msg.body('I am created by Kushal Bhavsar using Flask+NGRok and Twilio')
-    elif 'quote' in user_msg:
-        r = requests.get('https://api.quotable.io/random')
-        if r.status_code == 200:
-            data = r.json()
-            quote = f'{data["content"]} ({data["author"]})'
-        else:
-            quote = 'I could not retrieve a quote at this time, sorry.'
-        msg.body(quote)
-    elif 'cat' in user_msg:
-        msg.media('https://cataas.com/cat')
+        msg.body("Hi there! welcome to whatsapp chatbot! please select SUBJECT to download the material 1.DS 2.MEFA 3.DAA 4.WT 5.PC 6.ABOUT CHATBOT ")
+    elif '1' in user_msg:
+        msg.body('Download DS Material From Following Link - https://bit.ly/3xbTThS ')
+    elif '2' in user_msg:
+        msg.body("Download MEFA Material From Following Link - https://bit.ly/39dKuyd ")
+    elif '3' in user_msg:
+        msg.body("Download DAA Material From Following Link- https://bit.ly/39gaBob ")
+    elif '4' in user_msg:
+        msg.body('Download WT Material From Following Link-https://bit.ly/3O1YOZn')
+    elif '5' in user_msg:
+        msg.body('Download PC Material From Following Link - https://bit.ly/3H0gqCw ')
+    elif '6' in user_msg:
+        msg.body('All source code of this project github link - https://github.com/aadiaditya/chatbot  MADE WITH ❤️ BY SAI ADITYA')
     else:
-        msg.body("Sorry, I didn't get what you have said!")
+        msg.body("Sorry, Entered wrong option.Pls choose correct one")
     return str(bot_resp)
 
 if __name__ == '__main__':
